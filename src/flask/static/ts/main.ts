53562f94507019b469418738af0e300610d96c22
oonisidesu <eeeelllleeee24@gmail.com>
2024-06-02 04:05:13 +0900

    ESLintおよびPrettierの問題を修正: 未使用の変数を削除し、コードをフォーマットしました

--- src/flask/static/ts/main.ts
+++ src/flask/static/ts/main.ts
@@ -1,12 +1,15 @@
 function selectFolder(inputId: string) {
-  window.electron.selectFolder().then((folderPath: string) => {
-    const inputElement = document.getElementById(inputId) as HTMLInputElement;
-    if (inputElement) {
-      inputElement.value = folderPath;
-    } else {
-      console.error(`Element with id ${inputId} not found.`);
-    }
-  }).catch((err: Error) => {
-    console.log(err);
-  });
+  window.electron
+    .selectFolder()
+    .then((folderPath: string) => {
+      const inputElement = document.getElementById(inputId) as HTMLInputElement;
+      if (inputElement) {
+        inputElement.value = folderPath;
+      } else {
+        console.error(`Element with id ${inputId} not found.`);
+      }
+    })
+    .catch((err: Error) => {
+      console.log(err);
+    });
 }
